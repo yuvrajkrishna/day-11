4a04a1f9ae790608990a56cd9274616b82c790d0
yuvrajkrishna <yuvrajkrishna26@gmail.com>
2026-02-26 12:53:59 +0530

    seventh

--- subarray.java
+++ subarray.java
@@ -1,17 +1,19 @@
 public class subarray {
     public static void printSubarray(int arr[]){
-       
+        int ts = 0 ;
         for(int i = 0 ;i<arr.length;i++){
             int start = i;
             for(int j = i; j<arr.length;j++){
                 int end = j;
                 for(int k = start; k<=end;k++){
-                    System.out.print(arr[k] + " ");
+                    System.out.print(arr[k] + " "); 
                 }
+                ts++;
                 System.out.println();
             }
             System.out.println();
         }
+        System.out.println("Total subarray : " + ts);
     }
     public static void main(String[] args) {
         int []arr = {2,4,6,8,10};
