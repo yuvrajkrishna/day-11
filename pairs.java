9b02eb4fbee318d018f07e40f3b227c0c6232584
yuvrajkrishna <yuvrajkrishna26@gmail.com>
2026-02-26 11:15:16 +0530

    s

--- pairs.java
+++ pairs.java
@@ -1,11 +1,15 @@
 public class pairs{
     public static void printPairs(int []arr){
         for(int i = 0 ;i<arr.length;i++){
+            int tp = 0;
             int current = arr[i];
             for(int j = i+1;j<arr.length;j++){
                 System.out.print(current + "," + arr[j] + " ");
+                tp++;
             }
             System.out.println();
+
+            System.out.println("Total pairs for " + current + " is " + tp);
         }
     }
     public static void main(String[] args){
