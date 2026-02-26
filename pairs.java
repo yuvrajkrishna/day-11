86129d67590de9b9ffbc05be51e502ef77bd9198
yuvrajkrishna <yuvrajkrishna26@gmail.com>
2026-02-26 11:13:32 +0530

    f commit

---
+++ pairs.java
@@ -0,0 +1,15 @@
+public class pairs{
+    public static void printPairs(int []arr){
+        for(int i = 0 ;i<arr.length;i++){
+            int current = arr[i];
+            for(int j = i+1;j<arr.length;j++){
+                System.out.print(current + "," + arr[j] + " ");
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args){
+        int [] arr = {2,4,6,8,10};
+        printPairs(arr);
+    }
+}
